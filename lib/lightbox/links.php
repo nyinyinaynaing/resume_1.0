2178d252b77c1822cc80740b89cb09c6a5a8ea40
NAYNAING <naynaing.kollect@gmail.com>
2024-04-24 10:23:19 +0800

    first commit

---
+++ lib/lightbox/links.php
@@ -0,0 +1,17 @@
+<?php
+  $links = array(
+    'css' => 'lib/lightbox/css/lightbox.min.css',
+    'js' => 'lib/lightbox/js/lightbox.min.js'
+  );
+?>
+
+
+<!-- /******* 
+
+Design By               : MH RONY 
+Website URL             : https://developerrony.com
+GitHub URL              : https://github.com/dev-mhrony
+Yoututbe Channel Link   : https://m.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
+Yoututbe Channel Link   : https://www.youtube.com/@codecampbdofficial
+
+********/ -->
